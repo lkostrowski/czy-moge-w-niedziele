ee93ec1f5f42dbfdc04bd067133b40c786e71b89
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2018-05-07 18:57:22 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve('dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader'
+        }]
+    }
+};
